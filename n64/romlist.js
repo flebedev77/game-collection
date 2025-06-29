c9edc46e2392214ed5a3d00b609b4adcc6d6bd01
flebedev77 <flebedev99@gmail.com>
2025-06-29 19:10:08 +0930

    Added n64 games

---
+++ n64/romlist.js
@@ -0,0 +1,8 @@
+var ROMLIST = [
+  {url:"roms/mk64.z64",title:"Mario Kart 64"},
+  {url:"roms/sm64.z64",title:"Super Mario 64"},
+  {url:"roms/mp2.z64",title:"Mario Party 2"},
+  {url:"roms/dk.z64",title:"Donkey Kong 64"},
+  {url:"roms/ssb64.z64",title:"Super Smash Bros 64"},
+  {url:"roms/y64.z64",title:"Yoshi's Story 64"},
+];
